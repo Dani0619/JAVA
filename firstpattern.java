189f2c1711c35486ab0cd5214954677f55e3655e
Dani0619 <daniacode123@gmail.com>
2026-08-29 11:19:52 +0530

    first-pattern

--- firstpattern.java
+++ firstpattern.java
@@ -1,7 +1,10 @@
-class firstpattern{
+import java.util.Scanner;
+public class firstpattern{
     public static  void main(String[] args){
-        for(int i=0;i < 4;i++){
-            for(int j=0;j<4;j++){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i < n;i++){
+            for(int j=0;j<n;j++){
                 System.out.print("* ");
             }
             System.out.println();
