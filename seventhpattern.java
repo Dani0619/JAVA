96a8594fbade5d539d0b3c5488e97cf74db93ec2
Dani0619 <daniacode123@gmail.com>
2026-09-23 10:18:35 +0530

    seventh

--- seventhpattern.java
+++ seventhpattern.java
@@ -5,15 +5,15 @@ public class seventhpattern {
         int n= sc.nextInt();
         for(int i=0;i < n;i++){
             for(int j=0;j < n- i - 1;j++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for(int j=0; j < 2*i + 1;j++){
                 System.out.print("* ");
             }
             for(int j =0;j < n - i -1;j++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
-            System.out.println(" ");
+            System.out.println("  ");
         }
     }
     
